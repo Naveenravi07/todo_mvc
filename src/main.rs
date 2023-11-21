4e95687942bd82857f9896016aa39792d6a711b5
Naveenravi07 <gamerkidnav@gmail.com>
2023-11-21 23:29:06 +0530

    JSON form done

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,8 @@
 mod templates;
 use libsql_client::Config;
-use tower_http::{services::ServeDir, cors::CorsLayer};
-use axum::{ routing::{get, post}, Router, Json, http::{HeaderValue, Method, header::{AUTHORIZATION, ACCEPT, CONTENT_TYPE}}};
-use templates::{Index, Addtodo, CreateTodo};
+use tower_http::services::ServeDir;
+use axum::{ routing::get, Router, Json};
+use templates::{Index, Addtodo};
 
 async fn index() -> Index {
     return Index{} 
@@ -12,7 +12,7 @@ async fn addtodo() -> Addtodo {
     return Addtodo{} 
 }
 
-async fn createtodo(Json(form):Json<serde_json::Value>) -> axum::response::Html<String>{
+async fn createtodo(form:Json<serde_json::Value>) -> axum::response::Html<String>{
     println!("{:?}",form);
     format!(
         r#"
@@ -31,19 +31,12 @@ async fn createtodo(Json(form):Json<serde_json::Value>) -> axum::response::Html<
 
 #[tokio::main]
 async fn main() {
-    const DB_PATH : &str = "file:///home/shastri/balls/todo_mvc/todo.db";
+    const DB_PATH : &str = "file:///home/shastri/deez/todo_mvc/todo.db";
     let config = Config::new(DB_PATH).unwrap();
 
     let db = libsql_client::Client::from_config(config).await.unwrap();
     db.execute("CREATE TABLE IF NOT EXISTS todos (id INTEGER PRIMARY KEY AUTOINCREMENT, todo TEXT, completed BOOLEAN, priority TEXT, member TEXT)").await.unwrap();    
 
-
-
-    let cors = CorsLayer::new()
-        .allow_origin("http://localhost:3000".parse::<HeaderValue>().unwrap())
-        .allow_methods([Method::GET, Method::POST, Method::PATCH, Method::DELETE])
-        .allow_credentials(true)
-        .allow_headers([AUTHORIZATION, ACCEPT, CONTENT_TYPE]);
 
     let app = Router::new()
         .nest_service("/assets", ServeDir::new("assets"))
@@ -51,7 +44,7 @@ async fn main() {
         .route("/todo/add", get(addtodo).post(createtodo));
         
     axum::Server::bind(&"0.0.0.0:42069".parse().unwrap())
-        .serve(app.layer(cors).into_make_service())
+        .serve(app.into_make_service())
         .await
         .unwrap();
 }
