d600563a404c4df768acc3b55a8eb03b9e895a3d
Naveenravi07 <gamerkidnav@gmail.com>
2023-11-25 13:11:01 +0000

    Libsql__client downgraded for sync support

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,10 @@
 mod templates;
+use axum::{debug_handler, extract::State, routing::get, Form, Router};
+use libsql_client::{Client, Config, Statement, args};
 use std::sync::Arc;
-use axum::{extract::State, routing::get, Form, Router, debug_handler};
-use libsql_client::{Client, Config};
-use tokio::sync::Mutex;
 use templates::{Addtodo, CreateTodo, Index};
+use tokio::sync::Mutex;
 use tower_http::services::ServeDir;
-
 
 async fn index() -> Index {
     Index {}
@@ -19,14 +18,21 @@ async fn addtodo() -> Addtodo {
 async fn createtodo(
     State(state): State<AppState>,
     Form(data): Form<CreateTodo>,
-    ) -> axum::response::Html<String>{
+) -> axum::response::Html<String> {
 
-    println!("{:?}", data);
-    
-    state.database.lock().await.execute("INSERT INTO todos VALUES (1,'nuts',false,'mem','sasas')").await.unwrap();
+    state
+        .database
+        .lock()
+        .await
+        .execute(Statement::with_args(
+            "INSERT INTO todos (id,todo,member,priority,completed) VALUES (?,?,?,?,false)",
+            args!(data.id,data.todo,data.member,data.priority)
+            ))
+        .await
+        .unwrap();
 
     axum::response::Html(format!(
-            r#"
+        r#"
             <!doctype html>
             <html>
             <head>
@@ -38,7 +44,7 @@ async fn createtodo(
             </body>
             </html>
             "#
-            ))
+    ))
 }
 
 #[derive(Clone)]
@@ -51,12 +57,15 @@ async fn main() {
     const DB_PATH: &str = "file:///home/shastri/balls/todo_mvc/todo.db";
     let config = Config::new(DB_PATH).expect("Failed to create database configuration");
 
-    let db = Arc::new(Mutex::new(libsql_client::Client::from_config(config)
-        .await .expect("Failed to connect to the database")));
+    let db = Arc::new(Mutex::new(
+        libsql_client::Client::from_config(config)
+            .await
+            .expect("Failed to connect to the database"),
+    ));
     db.lock().await.execute("CREATE TABLE IF NOT EXISTS todos (id INTEGER PRIMARY KEY AUTOINCREMENT, todo TEXT, completed BOOLEAN, priority TEXT, member TEXT)").await.expect("Failed to create todos table");
-    db.lock().await.execute("INSERT INTO todos (id, todo, completed, priority, member) VALUES (1, 'nuts', false, 'mem', 'sasas')").await.unwrap();
+    //db.lock().await.execute("INSERT INTO todos (id, todo, completed, priority, member) VALUES (2, 'nuts', false, 'mem', 'sasas')").await.unwrap();
 
-    let app_state = AppState{database:db};
+    let app_state = AppState { database: db };
 
     let app = Router::new()
         .nest_service("/assets", ServeDir::new("assets"))
@@ -69,4 +78,3 @@ async fn main() {
         .await
         .expect("Server failed to start");
 }
-
