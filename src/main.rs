ab840cdf23e7da36bead960ab975b5f035ed8494
Naveenravi07 <gamerkidnav@gmail.com>
2023-11-25 13:16:43 +0000

    Slapped again

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,6 @@ use axum::{debug_handler, extract::State, routing::get, Form, Router};
 use libsql_client::{Client, Config, Statement, args};
 use std::sync::Arc;
 use templates::{Addtodo, CreateTodo, Index};
-use tokio::sync::Mutex;
 use tower_http::services::ServeDir;
 
 async fn index() -> Index {
@@ -22,8 +21,6 @@ async fn createtodo(
 
     state
         .database
-        .lock()
-        .await
         .execute(Statement::with_args(
             "INSERT INTO todos (id,todo,member,priority,completed) VALUES (?,?,?,?,false)",
             args!(data.id,data.todo,data.member,data.priority)
@@ -49,7 +46,7 @@ async fn createtodo(
 
 #[derive(Clone)]
 pub struct AppState {
-    pub database: Arc<Mutex<Client>>,
+    pub database: Arc<Client>,
 }
 
 #[tokio::main]
@@ -57,13 +54,12 @@ async fn main() {
     const DB_PATH: &str = "file:///home/shastri/balls/todo_mvc/todo.db";
     let config = Config::new(DB_PATH).expect("Failed to create database configuration");
 
-    let db = Arc::new(Mutex::new(
+    let db = Arc::new(
         libsql_client::Client::from_config(config)
             .await
             .expect("Failed to connect to the database"),
-    ));
-    db.lock().await.execute("CREATE TABLE IF NOT EXISTS todos (id INTEGER PRIMARY KEY AUTOINCREMENT, todo TEXT, completed BOOLEAN, priority TEXT, member TEXT)").await.expect("Failed to create todos table");
-    //db.lock().await.execute("INSERT INTO todos (id, todo, completed, priority, member) VALUES (2, 'nuts', false, 'mem', 'sasas')").await.unwrap();
+    );
+    db.execute("CREATE TABLE IF NOT EXISTS todos (id INTEGER PRIMARY KEY AUTOINCREMENT, todo TEXT, completed BOOLEAN, priority TEXT, member TEXT)").await.expect("Failed to create todos table");
 
     let app_state = AppState { database: db };
 
